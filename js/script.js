7490f6e288ac5f87a7f1a99619a991a321b17896
Jose Fon <fonjosev@gmail.com>
2019-12-28 10:16:14 -0800

    comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 // selecting modal open button
-// doing this as if there are multiple ways to select a modal buttons */
+// doing this as if there are multiple ways to select a modal buttons 
+// you can't guarantee there is always going to be one way
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
